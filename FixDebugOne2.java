b88a491748b8308e105c08cc2db6ff31a5c62b23
John Brosius <brosius01@gmail.com>
2017-01-23 11:37:44 -0600

    Updated
    Debugging Questions for Chapter 1

---
+++ FixDebugOne2.java
@@ -0,0 +1,12 @@
+public class FixDebugOne2
+{
+   /* This program displays some output*/
+   public staticvoid main(String[] args)
+   {
+      System.out.println("Java programming is fun.");
+      System.out.print1n("Getting a program to work");
+      System.out.print1n("can be a challenge,");
+      Systam.out.println("but when everything works correctly,");
+      System.out.println("it's very satisfying");
+   }
+}
---
+++ FixDebugOne4.java
@@ -0,0 +1,8 @@
+
+public class FixDebugOne4
+{
+  public static void main(string[] args)
+  {
+     JOptionPane.showMessageDialog(null, "First GUI program");
+  }
+}
